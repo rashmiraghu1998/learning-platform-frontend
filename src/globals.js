2a908ab07a03bde4f11c068f15e1228a6cf8e493
sharan98 <rudreshsharan@gmail.com>
2020-07-18 11:06:21 +0530

    Changes to new account - Use S3 instead of github pages - Change API URL

--- src/globals.js
+++ src/globals.js
@@ -1,5 +1,10 @@
 // window.url_prefix =
 //   "https://8yo72054p8.execute-api.us-west-2.amazonaws.com/api";
-window.url_prefix = "http://learning-platform-http-752640876.us-west-2.elb.amazonaws.com:5000";
 
+window.url_prefix =
+  "http://ec2-54-71-1-113.us-west-2.compute.amazonaws.com:5000";
+// "http://learning-platform-http-752640876.us-west-2.elb.amazonaws.com:5000";
+
+// window.url_prefix =
+//   "http://learning-platform-2a841ee60e8c9bcb.elb.us-west-2.amazonaws.com:5000";
 // window.frontend_url = "http://localhost:3000";
